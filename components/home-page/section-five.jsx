0af4ae7586841199125b2ad156763d883584820a
jimmys-box <89916651+jimmys-box@users.noreply.github.com>
2021-12-14 16:04:28 +0100

    fix: font file woff format added

--- components/home-page/section-five.jsx
+++ components/home-page/section-five.jsx
@@ -6,7 +6,6 @@ import { Box, Text, Button, Input, Image, Flex } from 'theme-ui';
 import { BsArrowRight } from "react-icons/bs";
 import Link from 'next/link'
 import { useTranslation } from "next-i18next";
-import PaypalButton from '../paypal-button';
 
 export default function SectionFive() {
     const { t } = useTranslation('homepage');
@@ -32,7 +31,6 @@ export default function SectionFive() {
                 <Box sx={styles.containerRight}>
 
                     <Box sx={styles.boxButton2}>
-{/* <PaypalButton /> */}
                         <Link href="https://www.helloasso.com/associations/mieux-voter" passHref>
                             <Button>{t("bouton cinquième section")}<BsArrowRight sx={styles.arrow} /></Button>
                         </Link> 
--- components/paypal-button.jsx
+++ components/paypal-button.jsx
@@ -5,7 +5,7 @@ import Script from 'next/script';
 
 export default function paypalButton() {
     const initialOptions = {
-        "client-id": "AQXcvapvn8rfiK3idXXfXdJLRnF42WYNUANbV9uj7bn1SbztIfYj2Q-l0v2JERL-imtZrbZC7Ai2Jikv",
+        "client-id": "your-id",
         currency: "EUR",
         intent: "capture",
         "data-client-token": "abc123xyz==",
@@ -13,7 +13,7 @@ export default function paypalButton() {
 
     return (
       
-        <PayPalScriptProvider options={{ "client-id": "AQXcvapvn8rfiK3idXXfXdJLRnF42WYNUANbV9uj7bn1SbztIfYj2Q-l0v2JERL-imtZrbZC7Ai2Jikv" }}>
+        <PayPalScriptProvider options={{ "client-id": "your-id" }}>
             <PayPalButtons style={{ layout: "horizontal", color: "blue" }} />
             <PayPalButtons style={{ layout: "horizontal", color: "white"}} />
             
