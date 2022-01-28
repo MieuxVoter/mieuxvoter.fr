d62a1d82b42a59940e822ed6d034132e3f13e4cb
jimmys-box <89916651+jimmys-box@users.noreply.github.com>
2022-01-28 15:05:17 +0100

    fix: arrows

--- components/with-video.jsx
+++ components/with-video.jsx
@@ -1,33 +1,28 @@
+
 import React from "react";
 import Carousel from "react-multi-carousel";
-import Video from "./video";
 
+import Video from "./Video";
 
 const responsive = {
-  desktop: {
-    breakpoint: {max: 3000, min: 1024},
-    items: 2,
-    paritialVisibilityGutter: 60
-  },
-  tablet: {
-    breakpoint: {max: 1024, min: 464},
-    items: 2,
-    paritialVisibilityGutter: 50
-  },
-  mobile: {
-    breakpoint: {max: 464, min: 0},
-    items: 1,
-    paritialVisibilityGutter: 30
+  doesntmatter: {
+    breakpoint: { max: 3000, min: 0 },
+    items: 1
   }
 };
-//
 
-const WithVideo = ({videos}) => (
-  <Carousel arrows={true} swipeable={true} draggable={true} responsive={responsive}>
-    {videos.map(({id, source, placeholder}, index) => {
-      return <Video id={id} key={index} placeholder={placeholder} source={source} />;
-    })}
-  </Carousel>
-)
+const videos = ["YC1xDJsi_Hk", "ZoGH7d51bvc"];
+
+class WithVideo extends React.Component {
+  render() {
+    return (
+      <Carousel swipeable={true} draggable={true} responsive={responsive}>
+        {videos.map(id => {
+          return <Video id={id} key={id} />;
+        })}
+      </Carousel>
+    );
+  }
+}
 
 export default WithVideo;
