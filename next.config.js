9bd50444dfd3e2dcae15715e8e5bd65571b2a8e1
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2022-03-13 13:14:19 +0100

    fix(ci): rm i18n

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
-const { i18n } = require("./next-i18next.config");
+const {i18n} = require("./next-i18next.config");
 
 module.exports = {
   i18n,
   // See https://github.com/netlify/netlify-plugin-nextjs/issues/223
-  unstableNetlifyFunctionsSupport: {
-    "pages/index.jsx": {
-      includeDirs: ["public"],
-    },
-    "pages/faq.jsx": {
-      includeDirs: ["public"],
-    },
-  },
-  pageExtensions: ["mdx", "jsx", "js", "ts", "tsx"],
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
+  // unstableNetlifyFunctionsSupport: {
+  //   "pages/index.jsx": {
+  //     includeDirs: ["public"],
+  //   },
+  //   "pages/faq.jsx": {
+  //     includeDirs: ["public"],
+  //   },
+  // },
+  // pageExtensions: ["mdx", "jsx", "js", "ts", "tsx"],
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: ["@svgr/webpack"],
+  //   });
 
-    return config;
-  }
+  //   return config;
+  // }
 };
