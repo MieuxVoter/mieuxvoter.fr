b2f090b783ecab7db446aaa60afa2f4830167736
Théo SABATTIÉ <theo@supercosi.com>
2025-01-12 14:56:20 +0100

    Fixed: Plot import

--- pages/presse/[tid].jsx
+++ pages/presse/[tid].jsx
@@ -1,11 +1,13 @@
-import {jsx, Text} from 'theme-ui';
+import {jsx, Box, Text} from 'theme-ui';
 import { MDXRemote } from 'next-mdx-remote';
 import { serialize } from 'next-mdx-remote/serialize';
 import dynamic from 'next/dynamic';
 import { useTranslation } from 'next-i18next';
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 
-const Plot = dynamic(() => import('react-plotly.js'), { ssr: false });
+const Plot = dynamic(import('react-plotly.js'), {
+  ssr: false
+})
 
 export async function getStaticPaths() {
     const {join} = require('path')
@@ -21,33 +23,73 @@ export async function getStaticPaths() {
   }
   
 export async function getStaticProps({ locale, params }) {
-    const matter = require('gray-matter');
-    const {join} = require('path')
-    const {readFileSync, cp} = require('fs')
+    const { join } = require('path')
+    const { readFileSync } = require('fs')
     const mdFolder = join(process.cwd(), 'content/presse')
     const filePath = join(mdFolder, `${params.tid}.mdx`);
     const fileContent = readFileSync(filePath, 'utf8')
-    const { data, content } = matter(fileContent);
-    const mdxSource = await serialize(content);
+    let mdxSource = await serialize(fileContent, {parseFrontmatter: true});
+    const scope = {}
+    
+    if (mdxSource.frontmatter.assets) {
+
+      Object.keys(mdxSource.frontmatter.assets).forEach(key => {
+        const value = mdxSource.frontmatter.assets[key]
+        const path = join(process.cwd(), 'content', 'assets', value.path)
+        const fileContents = readFileSync(path, 'utf8')
+        
+        if (value.type == 'json') {
+          scope[key] = JSON.parse(fileContents)
+        } else if (value.type == 'txt') {
+          scope[key] = fileContents
+        } else {
+          console.error('Unknown type')
+        }
+      })
+
+      mdxSource = await serialize(fileContent, {parseFrontmatter: true, scope: scope})
+    }
 
     return { props: { 
       ...(await serverSideTranslations(locale, ["presse", "common"])),
-      article: { ...data, mdxSource } } 
+      article: { mdxSource, scope } } 
     };
 }
 
 export default function PresseArticle({article}) {
   useTranslation('presse');
+
   return (
-    <section sx={styles.presse}>
-      <h1>{article.title}</h1>
-      <Text sx={styles.dateAndAuthor}>{article.date}, par {article.author}</Text>
-      <MDXRemote {...article.mdxSource} components={{ Plot }}/>
+    <section>
+      <Box sx={styles.blog}>
+        <h1>{article.mdxSource.frontmatter.title}</h1>
+        <Text sx={styles.dateAndAuthor}>{article.mdxSource.frontmatter.date}, par {article.mdxSource.frontmatter.author}</Text>
+        <Box sx={styles.post}>
+          <MDXRemote {...article.mdxSource} components={{ Plot }}/>
+        </Box>
+      </Box>
     </section>
   );
 }
 
 const styles = {
+  blog: {
+    pb: ['50px'],
+    h2: {
+      fontSize: ['52px', '72px'],
+      lineHeight: '1',
+      textAlign: 'left',
+
+    },
+    flexDirection: 'column',
+    margin: [0, '100px'],
+    mb: 0,
+    pl: ['5%', '5%', '5%', '7%'],
+    pb: ['100px'],
+  },
+  post: {
+    mt: '50px'
+  },
     containerThree: {
       width: [null, null, null, '90%', '80%'],
       mx: 'auto',
