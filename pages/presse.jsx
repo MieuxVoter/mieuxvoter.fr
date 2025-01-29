2af29adaff671598004545ca6d35be423ca6a347
Pierre Puchaud <40755537+Ipuch@users.noreply.github.com>
2025-01-29 10:44:43 -0500

    Added: Article about popularity measurement with MJ
    * article test
    
    * article modified
    
    * Changed: Link additive article to press
    
    * Fixed: plot import
    
    * Changed: sort articles by date (more recent first)
    
    * Fixed: localization on press article
    
    * Changed: Author and date on presse article
    
    * Fixed: france culture image on presse
    
    * Fixed: Plot import
    
    * Fixed: image to large (limit max width to the column)
    
    * update: 1er article
    
    * fusionnons comme ça.
    
    * delete: an extra width
    
    * fix: typo
    
    * resize imgs
    
    * maj the .jsx for blog posts
    
    ---------
    
    Co-authored-by: Théo SABATTIÉ <theo@supercosi.com>

--- pages/presse.jsx
+++ pages/presse.jsx
@@ -14,13 +14,41 @@ export const getStaticProps = async ({locale}) => {
 
   const {join} = require('path')
   const jsonData = join(process.cwd(), 'content/presse.json')
+  const mdFolder = join(process.cwd(), 'content/presse')
+
+  const files = require('fs').readdirSync(mdFolder);
+  const items = files.map((filename) => {
+    const filePath = join(mdFolder, filename);
+    const fileContents = readFileSync(filePath, 'utf8');
+    const { data, content } = matter(fileContents);
+    return { data, content, filename };
+  });
 
   const fileContents = readFileSync(jsonData, 'utf8')
   const {data, content} = matter(fileContents)
+
+  const articles = JSON.parse(content).concat(items.map(item => {
+    const data = item.data;
+    
+    return {
+      thumb:data.thumb, 
+      source: "Mieux Voter", 
+      altText: data.title,
+      title: data.title,
+      author: data.author,
+      date: data.date,
+      lien: `presse/${item.filename.replace(/\.mdx$/, '')}`
+    }
+  }));
+
+  articles.sort((a, b) => {
+    return new Date(b.date)-new Date(a.date); 
+  })
+
   return {
     props: {
       ...(await serverSideTranslations(locale, ["presse", "common"])),
-      items: JSON.parse(content)
+      items: articles,
     },
   }
 };
---
+++ pages/presse/[tid].jsx
@@ -0,0 +1,118 @@
+import {jsx, Box, Text} from 'theme-ui';
+import { MDXRemote } from 'next-mdx-remote';
+import { serialize } from 'next-mdx-remote/serialize';
+import dynamic from 'next/dynamic';
+import { useTranslation } from 'next-i18next';
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+
+const Plot = dynamic(import('react-plotly.js'), {
+  ssr: false
+})
+
+export async function getStaticPaths() {
+    const {join} = require('path')
+    const mdFolder = join(process.cwd(), 'content/presse')
+
+    const files = require('fs').readdirSync(mdFolder);
+    const paths = files.map((filename) => ({
+        params: { tid: filename.replace(/\.mdx$/, '') },
+    }));
+  
+    // We'll pre-render only these paths at build time.
+    return { paths, fallback: false };
+  }
+  
+export async function getStaticProps({ locale, params }) {
+    const { join } = require('path')
+    const { readFileSync } = require('fs')
+    const mdFolder = join(process.cwd(), 'content/presse')
+    const filePath = join(mdFolder, `${params.tid}.mdx`);
+    const fileContent = readFileSync(filePath, 'utf8')
+    let mdxSource = await serialize(fileContent, {parseFrontmatter: true});
+    const scope = {}
+    
+    if (mdxSource.frontmatter.assets) {
+
+      Object.keys(mdxSource.frontmatter.assets).forEach(key => {
+        const value = mdxSource.frontmatter.assets[key]
+        const path = join(process.cwd(), 'content', 'assets', value.path)
+        const fileContents = readFileSync(path, 'utf8')
+        
+        if (value.type == 'json') {
+          scope[key] = JSON.parse(fileContents)
+        } else if (value.type == 'txt') {
+          scope[key] = fileContents
+        } else {
+          console.error('Unknown type')
+        }
+      })
+
+      mdxSource = await serialize(fileContent, {parseFrontmatter: true, scope: scope})
+    }
+
+    return { props: { 
+      ...(await serverSideTranslations(locale, ["presse", "common"])),
+      article: { mdxSource, scope } } 
+    };
+}
+
+export default function PresseArticle({article}) {
+  useTranslation('presse');
+
+  return (
+    <section>
+      <Box sx={styles.blog}>
+        <h1>{article.mdxSource.frontmatter.title}</h1>
+        <Text sx={styles.dateAndAuthor}>{article.mdxSource.frontmatter.date}, par {article.mdxSource.frontmatter.author}</Text>
+        <Box sx={styles.post}>
+          <MDXRemote {...article.mdxSource} components={{ Plot, img: (props) => <img {...props} style={{ maxWidth: '100%' }} /> }}/>
+        </Box>
+      </Box>
+    </section>
+  );
+}
+
+const styles = {
+  blog: {
+    pb: ['50px'],
+    h2: {
+      fontSize: ['52px', '72px'],
+      lineHeight: '1',
+      textAlign: 'left',
+
+    },
+    flexDirection: 'column',
+    margin: ['auto'],
+    mb: 0,
+    pl: ['5%', '5%', '5%', '7%'],
+    pb: ['100px'],
+    "max-width": '960px',
+  },
+  post: {
+    mt: '50px',
+    display: 'grid',
+    "place-items": 'center'
+  },
+    containerThree: {
+      width: [null, null, null, '90%', '80%'],
+      mx: 'auto',
+    },
+    sectionThree: {
+      py: 9,
+      h2: {
+        fontSize: ['52px', '72px'],
+        lineHeight: '1',
+        textAlign: 'left',
+        ml: ['10%', '15%', '5%'],
+      },
+    },
+    sectionTitle: {
+      width: ['100%', '100%', '100%', '20%'],
+    },
+    dateAndAuthor: {
+      fontSize: 2,
+      color: 'grey',
+      fontStyle: 'italic',
+      fontSize: '13px',
+   },
+  }
